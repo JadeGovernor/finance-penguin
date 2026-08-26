675aeae50492e3e1d0f22f5247c9307afc1ceedd
Che Wang <wdz732193013@gmail.com>
2026-08-26 22:06:19 +0800

    deploy: Finance Penguin 静态站点（GitHub Pages）

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { Route, Routes } from 'react-router'
-import HomePage from '@/pages/HomePage'
-
-export default function App() {
-  return <Routes><Route path="/" element={<HomePage />} /></Routes>
-}
